6604813b3c9a645e1be4dff17062628469c22436
Martin Riedl <admin@martin-riedl.de>
2010-05-14 19:25:46 +0200

    NEW: Server-Config-File can now be reloaded FIX: list of tables for reload are now sorted UPDATE: Debug-Function is now splitted into 2 levels NEW: every button has now a tooltip with detailed informations

--- Frame.lua
+++ Frame.lua
@@ -1,61 +1,78 @@
 ﻿SLASH_MADDINSMANGADMIN1 = "/maddinsmangadmin";
 -- use "/maddinsmangadmin debug" for debug-informations
+-- use "/maddinsmangadmin debug_extended" for extended debug-informations
 
-version = "2010.05.13";
-debugFlag = false;
+version = "2010.05.14";
+debugFlag = 0;
 
 reloadTables = {
-	["01"] = {
+	["all"] = {
 		["text"] = "all",
 		["command"] = ".reload all"
 	},
-	["02"] = {
+	["achievement"] = {
 		["text"] = "achievement",
 		["command"] = ".reload all_achievement"
 	},
-	["03"] = {
+	["area"] = {
 		["text"] = "area",
 		["command"] = ".reload all_area"
 	},
-	["04"] = {
+	["eventai"] = {
 		["text"] = "eventai",
 		["command"] = ".reload all_eventai"
 	},
-	["05"] = {
+	["item"] = {
 		["text"] = "item",
 		["command"] = ".reload all_item"
 	},
-	["06"] = {
+	["locales"] = {
 		["text"] = "locales",
 		["command"] = ".reload all_locales"
 	},
-	["07"] = {
+	["loot"] = {
 		["text"] = "loot",
 		["command"] = ".reload all_loot"
 	},
-	["08"] = {
+	["npc"] = {
 		["text"] = "npc",
 		["command"] = ".reload all_npc"
 	},
-	["09"] = {
+	["quest"] = {
 		["text"] = "quest",
 		["command"] = ".reload all_quest"
 	},
-	["10"] = {
+	["scripts"] = {
 		["text"] = "scripts",
 		["command"] = ".reload all_scripts"
 	},
-	["11"] = {
+	["spell"] = {
 		["text"] = "spell",
 		["command"] = ".reload all_spell"
 	}
 };
 
+reloadTablesOrder = {
+	"all",
+	"achievement",
+	"area",
+	"eventai",
+	"item",
+	"locales",
+	"loot",
+	"npc",
+	"quest",
+	"scripts",
+	"spell"
+};
+
 SlashCmdList["MADDINSMANGADMIN"] = function(msg)
 	if msg == "debug" then
-		debugFlag = true;
+		debugFlag = 1;
+	elseif msg == "debug_extended" then
+		debugFlag = 2;
 	else
-		debugFlag = false;
+		debugFlag = 0;
 	end
 	showMainFrame();
 end
@@ -68,12 +85,19 @@ function showMainFrame()
 	FontStringVersion:SetText("Version: "..version);
 	
 	ButtonHide.tooltipText = "Schließt das Fenster";
+	
+	ButtonServermessageSend.tooltipText = "Sendet den eingegebenen Text an alle Spieler die gerade online sind";
+	ButtonServerrestart.tooltipText = "Startet den Server nach x Sekunden neu";
+	ButtonServerShutdown.tooltipText = "Fährt den Server nach x Sekunden herunter";
 	ButtonServerexit.tooltipText = "Beenden den Server sofort";
 	ButtonServerrestartCancel.tooltipText = "Bricht den Neustart des Servers ab";
 	ButtonServershutdownCancel.tooltipText = "Bricht das Herunterfahren des Servers ab";
+	ButtonServerMotdSave.tooltipText = "Ändert die Nachricht des Tages";
+	ButtonServerMotdShow.tooltipText = "Zeigt die aktuelle Nachricht des Tages im Chat-Fenster an";
 	ButtonServerReload.tooltipText = "Lädt die ausgewählte Tabelle(n) neu";
+	ButtonServerReloadConfigFile.tooltipText = "Lädt die Serverkonfigurationsdatei neu";
 	
-	alert("show frame ready");
+	alert("show frame ready", 1);
 end
 
 function ButtonHide_OnClick()
@@ -85,39 +109,35 @@ function hideAllTabs()
 	TabGeneral:Hide();
 	TabServer:Hide();
 	
-	-- enable all buttons
-	-- ButtonTabGeneral:Enable();
-	-- ButtonTabServer:Enable();
+	-- enable all tabs
 	PanelTemplates_DeselectTab(ButtonTabGeneral);
 	PanelTemplates_DeselectTab(ButtonTabServer);
-	alert("all tabs are now hidden");
+	alert("all tabs are now hidden", 1);
 end
 
 function ButtonTabGeneral_OnClick()
 	hideAllTabs();
 	TabGeneral:Show();
-	-- ButtonTabGeneral:Disable();
 	PanelTemplates_SelectTab(ButtonTabGeneral);
-	alert("show tab general ready");
+	alert("show tab general ready", 1);
 end
 
 function ButtonTabServer_OnClick()
 	hideAllTabs();
 	TabServer:Show();
-	-- ButtonTabServer:Disable();
 	PanelTemplates_SelectTab(ButtonTabServer);
-	alert("show tab server");
+	alert("show tab server", 1);
 end
 
 function ButtonServermessageSend_OnClick()
 	if EditBoxServermessage:GetText() ~= "" then
-		alert("send server message");
+		alert("send server message", 1);
 		command = ".announce "..EditBoxServermessage:GetText();
 		executeCommand(command);
 		command = ".notify "..EditBoxServermessage:GetText();
 		executeCommand(command);
 	else
-		alert("no message to send");
+		alert("no message to send", 1);
 	end
 	
 	EditBoxServermessage:SetText("");
@@ -126,11 +146,11 @@ end
 
 function ButtonServerrestart_OnClick()
 	if EditBoxServertime:GetText() ~= "" then
-		alert("restart server in "..EditBoxServertime:GetText().." seconds");
+		alert("restart server in "..EditBoxServertime:GetText().." seconds", 1);
 		command = ".server restart "..EditBoxServertime:GetText();
 		executeCommand(command);
 	else
-		alert("no time set");
+		alert("no time set", 1);
 	end
 	
 	EditBoxServertime:SetText("");
@@ -139,11 +159,11 @@ end
 
 function ButtonServershutdown_OnClick()
 	if EditBoxServertime:GetText() ~= "" then
-		alert("shutdown server in "..EditBoxServertime:GetText().." seconds");
+		alert("shutdown server in "..EditBoxServertime:GetText().." seconds", 1);
 		command = ".server shutdown "..EditBoxServertime:GetText();
 		executeCommand(command);
 	else
-		alert("no time set");
+		alert("no time set", 1);
 	end
 	
 	EditBoxServertime:SetText("");
@@ -151,22 +171,22 @@ function ButtonServershutdown_OnClick()
 end
 
 function ButtonServerexit_OnClick()
-	alert("server exit");
+	alert("server exit", 1);
 	executeCommand(".server exit");
 end
 
 function ButtonServerrestartCancel_OnClick()
-	alert("cancel server restart");
+	alert("cancel server restart", 1);
 	executeCommand(".server restart cancel");
 end
 
 function ButtonServershutdownCancel_OnClick()
-	alert("cancel server shutdown");
+	alert("cancel server shutdown", 1);
 	executeCommand(".server shutdown cancel");
 end
 
 function ButtonServerMotdSave_OnClick()
-	alert("save new message of the day");
+	alert("save new message of the day", 1);
 	command = ".server set motd "..EditBoxMotd:GetText();
 	executeCommand(command);
 	
@@ -175,16 +195,16 @@ function ButtonServerMotdSave_OnClick()
 end
 
 function ButtonServerMotdShow_OnClick()
-	alert("show message of the day");
+	alert("show message of the day", 1);
 	executeCommand(".server motd");
 end
 
 function DropDownMenuTables_OnLoad()
-	for key, subarray in pairs(reloadTables) do
+	for key, subarray in pairs(reloadTablesOrder) do
 		info		= {};
-		info.text	= reloadTables[key].text;
-		info.value	= key;
-		info.func	= DropDownMenuTables_Selected
+		info.text	= reloadTables[reloadTablesOrder[key]].text;
+		info.value	= reloadTablesOrder[key];
+		info.func	= DropDownMenuTables_Selected;
 		UIDropDownMenu_AddButton(info);
 	end
 end
@@ -196,11 +216,17 @@ end
 function ButtonServerReload_OnClick()
 	if UIDropDownMenu_GetSelectedValue(DropDownMenuTables) then
 		executeCommand(reloadTables[UIDropDownMenu_GetSelectedValue(DropDownMenuTables)].command);
+	else
+		alert("no table selected", 1);
 	end
 end
 
+function ButtonServerReloadConfigFile_OnClick()
+	executeCommand(".reload config");
+end
+
 function MouseOverShow(self)
-	alert("show mouveover");
+	alert("show mouveover", 2);
 	GameTooltip:SetOwner(self, "ANCHOR_RIGHT");
 	GameTooltip:SetText(self:GetText(), 1, 1, 1);
 	GameTooltip:AddLine(self.tooltipText);
@@ -208,19 +234,19 @@ function MouseOverShow(self)
 end
 
 function MouseOverHide(self)
-	alert("hide mouseover");
+	alert("hide mouseover", 2);
 	GameTooltip:Hide();
 end
 
 function executeCommand(command)
-	alert("Execute: "..command);
+	alert("Execute: "..command, 1);
 	--SendChatMessage(command, "CHANNEL", nil, 1);
 	SendChatMessage(command, "SAY", nil, nil);
-	alert("Execution successfull");
+	alert("Execution successfull", 1);
 end
 
-function alert(msg)
-	if debugFlag == true then
+function alert(msg, level)
+	if level <= debugFlag then
 		print(msg);
 	end
 end
